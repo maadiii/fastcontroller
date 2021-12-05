510cfc794c441e3f4ae4873369c3c5d0346a47da
mehdiazizi <mehdiiaziizii@gmail.com>
2021-12-05 08:16:52 +0330

    Make config exportable

--- config.go
+++ config.go
@@ -2,71 +2,17 @@ package fastcontroller
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 
 	"github.com/golang-jwt/jwt"
-	"github.com/sirupsen/logrus"
 )
 
 var (
-	jwtAlgorithms = map[string]*jwt.SigningMethodHMAC{
+	JwtAlgorithms = map[string]*jwt.SigningMethodHMAC{
 		"HS256": jwt.SigningMethodHS256,
 		"HS384": jwt.SigningMethodHS384,
 		"HS512": jwt.SigningMethodHS512,
 	}
 )
-
-func NewConfig() Config {
-	sc := SessionConfig{
-		Driver:          "postgres",
-		Host:            os.Getenv("POSTGRES_HOST"),
-		Port:            os.Getenv("POSTGRES_PORT"),
-		User:            os.Getenv("POSTGRES_USER"),
-		Password:        os.Getenv("POSTGRES_PASSWORD"),
-		DBName:          os.Getenv("POSTGRES_DB"),
-		Schema:          os.Getenv("POSTGRES_SCHEMA"),
-		TestDBName:      "kariz_test",
-		AdminDBName:     "postgres",
-		SslMode:         "disable",
-		TimeZone:        "Asia/Tehran",
-		MigrationsPath:  "file://migrations",
-		MigrationsTable: "migration_andsm",
-	}
-
-	maxAge, err := strconv.ParseInt(os.Getenv("JWT_MAXAGE"), 10, 64)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	jwtConfig := JWT{
-		Secret:    []byte(os.Getenv("JWT_SECRET_KEY")),
-		Algorithm: jwtAlgorithms[os.Getenv("JWT_ALGORITHM")],
-		MaxAge:    maxAge,
-		HTTPOnly:  os.Getenv("JWT_HTTPONLY") == "true",
-	}
-
-	cfg := Config{
-		HTTPPort:       9000,
-		DbSession:      sc,
-		DockerRegistry: "reg.bernetco.ir",
-		Agent: agent{
-			Port: 6000,
-			API: agentApis{
-				Start: "api/sessions/start",
-				Stop:  "api/session/stop",
-			},
-		},
-		JWT:       jwtConfig,
-		Templates: os.Getenv("TEMPLATES"),
-	}
-	mod := os.Getenv("ANDSM_ENV")
-	if mod != "release" {
-		logrus.Warning("Application run in DEVELOPER mode, set ANDSM_ENV to release for production mod")
-		cfg.DevMode = true
-	}
-
-	return cfg
-}
 
 type Config struct {
 	DevMode        bool
@@ -75,16 +21,16 @@ type Config struct {
 	HTTPPort       int
 	DbSession      SessionConfig
 	DockerRegistry string
-	Agent          agent
+	Agent          Agent
 	Templates      string
 }
 
-type agent struct {
+type Agent struct {
 	Port uint16
-	API  agentApis
+	API  AgentApis
 }
 
-type agentApis struct {
+type AgentApis struct {
 	Start string
 	Stop  string
 }
